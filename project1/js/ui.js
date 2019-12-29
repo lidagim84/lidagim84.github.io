c085f534f010edee4fbc5988cf361cb42c7305c2
lida <leaderek@naver.com>
2019-12-29 22:36:31 +0900

    ing

---
+++ project1/js/ui.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    
+    $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: false,
+    nextArrow: false
+    });
+
+});
