911422073a8e1a593f0430195595e7ebe1a68270
lida <leaderek@naver.com>
2019-12-31 00:05:22 +0900

    update

---
+++ project1/takeone_admin/js/admin.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    mTab();
+
+
+
+
+});
+
+
+//mTab
+function mTab() {
+    $('.eTab').each(function(){
+        var selected = $(this).find('> ul > li.selected > a');
+        if(selected.siblings('ul').length <= 0){
+            $(this).removeClass('gExtend');
+        }
+    });
+    $('body').delegate('.eTab a', 'click', function(e){
+        var _li = $(this).parent('li').addClass('selected').siblings().removeClass('selected'),
+            _target = $(this).attr('href'),
+            _siblings = $(_target).attr('class');
+        if(_siblings){
+            var _arr = _siblings.split(" "),
+                _classSiblings = '.'+_arr[0];
+            $(_target).show().siblings(_classSiblings).hide();
+        }
+        e.preventDefault();
+    });
+}
+
