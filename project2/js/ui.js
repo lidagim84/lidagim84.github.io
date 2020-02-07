74446afa9437190199ba91ddce198918c2b169fe
lidagim84 <56247582+lidagim84@users.noreply.github.com>
2020-02-07 11:07:49 +0900

    2/7

--- project2/js/ui.js
+++ project2/js/ui.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     eLnb();
+    eTab();
 
 
  
@@ -25,5 +26,34 @@ function eLnb() {
     })
 }
 
+//tab
+function eTab() {
+    $('.eTab').each(function(){
+        var $selected, $content, $links = jQuery(this).find('a');
+
+        var $selected = jQuery($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+        $selected.parent('li').addClass('selected');
+
+        $content = $($selected[0].hash);
+
+        // Hide the remaining content
+        $links.not($selected).each(function () {
+            jQuery(this.hash).hide();
+        });
+
+        jQuery(this).on('click', 'a', function(e){
+            $selected.parent('li').removeClass('selected');
+            $content.hide();
+
+            $selected = jQuery(this);
+            $content = jQuery(this.hash);
+
+            $selected.parent('li').addClass('selected');
+            $content.show();
+
+            e.preventDefault();
+        });
+    });
+}
 
 
