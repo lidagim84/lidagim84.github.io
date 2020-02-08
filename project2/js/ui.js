8f7a08c413500557149e827aad815b9e818c2963
lida <leaderek@naver.com>
2020-02-08 20:42:55 +0900

    2/8

--- project2/js/ui.js
+++ project2/js/ui.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     eLnb();
     eTab();
+    eDropDown();
 
 
  
@@ -56,4 +57,52 @@ function eTab() {
     });
 }
 
+//dropdown
+function eDropDown() {
+    $(".eDropDown").click(function () {
+        var parent = $(this).parent();
+
+        if (parent.hasClass('selected')) {
+            parent.removeClass("selected");
+        }else {
+            parent.siblings().removeClass("selected")
+            parent.addClass("selected");
+        }
+    })
+}
+
+
+// ※ 개발작업시 datepicker 플러그인 수정요청: 최상위에 mCalendar 클래스를 추가해주세요.
+$( function() {
+    //한글설정
+    $.datepicker.regional['ko'] = {
+        closeText: '닫기',
+        prevText: '이전달',
+        nextText: '다음달',
+        currentText: '오늘',
+        monthNames: ['1월','2월','3월','4월','5월','6월',
+            '7월','8월','9월','10월','11월','12월'],
+        monthNamesShort: ['1월','2월','3월','4월','5월','6월',
+            '7월','8월','9월','10월','11월','12월'],
+        dayNames: ['일','월','화','수','목','금','토'],
+        dayNamesShort: ['일','월','화','수','목','금','토'],
+        dayNamesMin: ['일','월','화','수','목','금','토'],
+        weekHeader: 'Wk',
+        dateFormat: 'mm.dd', //'yy.mm.dd'
+        firstDay: 1,
+        isRTL: false,
+        duration:200,
+        showAnim:'show',
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ko']);
+    $( ".datepicker" ).datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
+    $('.type_cal .form_cal').on('click', function() {
+        $(this).find('.datepicker').datepicker('show');
+    });
+
+} );
 
